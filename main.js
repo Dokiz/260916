2782cd21cba9d28f234ad9cff19ab0297dfa833c
Dokiz_228@mail.ru <deoptgqqr12>
2016-09-26 15:27:22 +0300

    start

--- main.js
+++ main.js
@@ -1,9 +1,3 @@
-var triangle={
-	ab:3,
-	bc:4,
-	ac:function(){
-		sss = (this.ab*this.ab)+(this.bc*this.bc);
-		return Math.sqrt(sss);
-	}
-};
-console.log(triangle.ac());
+function kw(a,b,c){
+	if a=0
+}
